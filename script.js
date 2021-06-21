eb2741cd0e046515a0f1d284d326e73ea606da98
Alec Cutajar <aleccutajar@gmail.com>
2021-06-21 11:37:15 -0400

    conversion to class from constructors

--- script.js
+++ script.js
@@ -28,11 +28,13 @@ function addBook() {
 
 }
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    };
 }
 
 function addBookToLibrary(book) {
